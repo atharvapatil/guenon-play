4e38033f181822fcdfd8a157f9a17f10f1b4b679
atharvapatil <atharvaabhaypatil@gmail.com>
2019-05-01 12:05:06 -0400

    added assests & enumerationcases

--- final/sketch.js
+++ final/sketch.js
@@ -1,9 +1,8 @@
 //References
 //https://p5js.org/reference/#/p5.Element/id
-// https://p5js.org/reference/#/p5/createButton
 // https://p5js.org/reference/#/p5/createSlider
 
-//Prevent page scrolling
+//Prevent default page scrolling
 // document.ontouchmove = function(event){
 //   event.preventDefault();
 // }
@@ -17,19 +16,49 @@ let img, img2, img3;
 // Enumerations variable
 let evolutionCase;
 
+let monkeyBrown1, monkeyBrown2, monkeyBrown3, monkeyBrown4, monkeyBrown5;
+let monkeyYellow1, monkeyYellow2, monkeyYellow3, monkeyYellow4, monkeyYellow5;
+let monkeyRed1,  monkeyRed2,  monkeyRed3,  monkeyRed4,  monkeyRed5;
+let monkeyPink1,  monkeyPink2,  monkeyPink3,  monkeyPink4,  monkeyPink5;
+
 function preload(){
-  img = loadImage('test.svg');
-  img2 = loadImage('test.svg');
-  img3 = loadImage('test.svg');
+
+  monkeyRed1 = loadImage('/images/monkeyRed1.svg');
+  monkeyRed2 = loadImage('/images/monkeyRed2.svg');
+  monkeyRed3 = loadImage('/images/monkeyRed3.svg');
+  monkeyRed4 = loadImage('/images/monkeyRed4.svg');
+  monkeyRed5 = loadImage('/images/monkeyRed5.svg');
+
+  monkeyYellow1 = loadImage('/images/monkeyYellow1.svg');
+  monkeyYellow2 = loadImage('/images/monkeyYellow2.svg');
+  monkeyYellow3 = loadImage('/images/monkeyYellow3.svg');
+  monkeyYellow4 = loadImage('/images/monkeyYellow4.svg');
+  monkeyYellow5 = loadImage('/images/monkeyYellow5.svg');
+
+  monkeyRed1 = loadImage('/images/monkeyRed1.svg');
+  monkeyRed2 = loadImage('/images/monkeyRed2.svg');
+  monkeyRed3 = loadImage('/images/monkeyRed3.svg');
+  monkeyRed4 = loadImage('/images/monkeyRed4.svg');
+  monkeyRed5 = loadImage('/images/monkeyRed5.svg');
+
+  monkeyBrown1 = loadImage('/images/monkeyBrown1.svg');
+  monkeyBrown2 = loadImage('/images/monkeyBrown2.svg');
+  monkeyBrown3 = loadImage('/images/monkeyBrown3.svg');
+  monkeyBrown4 = loadImage('/images/monkeyBrown4.svg');
+  monkeyBrown5 = loadImage('/images/monkeyBrown5.svg');
+
+  img = loadImage('/images/test.svg');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  // Monkey slider
   sliderMonkeys = createSlider(0, 5, 0);
   sliderMonkeys.position(width - 200 - sliderMonkeys.width, height/2 );
   sliderMonkeys.id('slider');
 
+  // Evolution slider
   sliderEvolution = createSlider(1, 5, 1);
   sliderEvolution.position(width - 200 - sliderEvolution.width, height/2 + 50);
   sliderEvolution.id('slider');
@@ -38,7 +67,8 @@ function setup() {
 
 
 function draw() {
-  background(40);
+
+  drawBG();
 
   // Check for the
   monkeySliderValue = sliderMonkeys.value();
@@ -56,59 +86,198 @@ function draw() {
 
   switch (evolutionCase){
     case 1:
-      levelOne();
+      handleCaseOne();  // No other monkeys
       console.log(evolutionCase);
       break;
     case 2:
-      levelTwo();
+      handleCaseTwo();  // One other monkey and no evolution
       console.log(evolutionCase);
       break;
     case 3:
-      levelThree();
+      handleCaseThree();
       console.log(evolutionCase);
       break;
+    case 4:
+      handleCaseFour();
+      console.log(evolutionCase);
+      break;
+    case 5:
+      handleCaseFive();
+      console.log(evolutionCase);
+      break;
+    case 6:
+      handleCaseSix();
+      console.log(evolutionCase);
+      break;
+    case 7:
+      handleCaseSeven();
+      console.log(evolutionCase);
+      break;
+    case 8:
+      handleCaseEight();
+      console.log(evolutionCase);
+      break;
+    case 9:
+      handleCaseNine();
+      console.log(evolutionCase);
+      break;
+    case 10:
+      handleCaseTen();
+      console.log(evolutionCase);
+      break;
+    case 11:
+      handleCaseEleven();
+      console.log(evolutionCase);
+      break;
+    case 12:
+      handleCaseTwelve();
+      console.log(evolutionCase);
+      break;
+    case 13:
+      handleCaseThirteen();
+      console.log(evolutionCase);
+      break;
+    case 14:
+      handleCaseFourteen();
+      console.log(evolutionCase);
+      break;
+    case 15:
+      handleCaseFifteen();
+      console.log(evolutionCase);
+      break;
+    case 16:
+      handleCaseSixteen();
+      console.log(evolutionCase);
+      break;
+    case 17:
+      handleCaseSeventeen();
+      console.log(evolutionCase);
+      break;
+    case 18:
+      handleCaseEighteen();
+      console.log(evolutionCase);
+      break;
+    case 19:
+      handleCaseNineteen();
+      console.log(evolutionCase);
+      break;
+    case 20:
+      handleCaseTwenty();
+      console.log(evolutionCase);
+      break;
+    case 21:
+      handleCaseTwentyOne();
+      console.log(evolutionCase);
+      break;
+
     default:
       fill(255);
-      textSize(24);
-      textAlign(CENTER);
-      text("Welcome to reality, Agent Smith", width/2, height/2);
-      // console.log(monkeySliderValue '+' evolutionSliderValue);
+      text("Evolution cases broke", width/2, height/2);
   }
-
-
-
 
 }
 
-function levelOne(){
-  tint(140, 45, 200);
-  image(img, 100, 100, 50, 60);
+function drawBG(){
+    background(40);
+}
+
+// No other monkeys
+function handleCaseOne(){
+
+  image(img, 100, 100, 150, 176);
   r = 255;
   g = 20;
   b = 255;
 }
 
-function levelTwo(){
-  tint(140,45, 200);
+// 1 monkey and 0 evolution
+function handleCaseTwo(){
+
   image(img, 100, 100, 50, 60);
-  tint(40, 145, 200);
-  image(img2, 100, 300, 90, 60);
-  r = 10;
-  g = 255;
-  b = 10;
+  image(img, 100, 300, 90, 60);
 }
 
-function levelThree(){
-  tint(40, 145, 200);
+function handleCaseThree(){
+
   image(img, 100, 100, 50, 60);
-  tint(140, 45, 200);
-  image(img2, 100, 300, 90, 60);
-  tint(0, 245, 200);
-  image(img3, 400, 300, 30, 60);
-  r = 255;
-  g = 10;
-  b = 10;
+  image(img, 100, 300, 90, 60);
+  image(img, 400, 300, 30, 60);
+
 }
+
+function handleCaseFour(){
+
+}
+
+function handleCaseFive(){
+
+}
+
+function handleCaseSix(){
+
+}
+
+function handleCaseSeven(){
+
+}
+
+function handleCaseEight(){
+
+}
+
+function handleCaseNine(){
+
+}
+
+function handleCaseTen(){
+
+}
+
+function handleCaseEleven(){
+
+}
+
+function handleCaseTwelve(){
+
+}
+
+function handleCaseThirteen(){
+
+}
+
+function handleCaseFourteen(){
+
+}
+
+function handleCaseFifteen(){
+
+}
+
+function handleCaseSixteen(){
+
+}
+
+
+function handleCaseSeventeen(){
+
+}
+
+function handleCaseEighteen(){
+
+}
+
+function handleCaseNineteen(){
+
+}
+
+function handleCaseTwenty(){
+
+}
+
+function handleCaseTwentyOne(){
+
+}
+
 
 // Evolution Enumerations
 
@@ -159,4 +328,5 @@ function evolutionEnumerations(){
   } else{
     evolutionCase = 0;
   }
+
 }
